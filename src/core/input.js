2e10d57d023b5b0581f14fdbccaad476e559bb83
duduxiong <xiang_linzong@163.com>
2019-01-29 11:44:21 +0800

    initialized: project

--- src/core/input.js
+++ src/core/input.js
@@ -1,22 +1,24 @@
+// 输入类，其中参数value表示输入数值；而size表示对输入数值的维度裁剪
+// 如果size表示数组，则构造表示三维结构的数组形式；如果size为对象，则同样构造为表示三维结构的数组形式
 module.exports = class Input {
-	constructor(value, size) {
-		this.value = value;
-		if (Array.isArray(size)) {
-			this.size = [];
-			for (let i = 0; i < size.length; i++) {
-				this.size[i] = size[i];
-			}
-			while (this.size.length < 3) {
-				this.size.push(1);
-			}
-		} else {
-			if (size.z) {
-				this.size = [size.x, size.y, size.z];
-			} else if (size.y) {
-				this.size = [size.x, size.y, 1];
-			} else {
-				this.size = [size.x, 1, 1];
-			}
-		}
-	}
+  constructor(value, size) {
+    this.value = value;
+    if (Array.isArray(size)) {
+      this.size = [];
+      for (let i = 0; i < size.length; i++) {
+        this.size[i] = size[i];
+      }
+      while (this.size.length < 3) {
+        this.size.push(1);
+      }
+    } else {
+      if (size.z) {
+        this.size = [size.x, size.y, size.z];
+      } else if (size.y) {
+        this.size = [size.x, size.y, 1];
+      } else {
+        this.size = [size.x, 1, 1];
+      }
+    }
+  }
 };
--- src/core/texture.js
+++ src/core/texture.js
@@ -1,57 +1,54 @@
 'use strict';
 
 module.exports = class Texture {
+  /**
+   *
+   * @param {*} texture 纹理图片
+   * @param {*} size 纹理图片尺寸
+   * @param {*} dimensions 纹理映射维度
+   * @param {*} output 纹理映射输出
+   * @param {*} webGl WebGL上下文环境
+   * @param {*} type 纹理映射类型
+   */
+  constructor(
+    texture,
+    size,
+    dimensions,
+    output,
+    webGl,
+    type = 'NumberTexture'
+  ) {
+    this.texture = texture;
+    this.size = size;
+    this.dimensions = dimensions;
+    this.output = output;
+    this.webGl = webGl;
+    this.kernel = null;
+    this.type = type;
+  }
 
-	/**
-	 * @desc WebGl Texture implementation in JS
-	 * @constructor Texture
-	 * @param {Object} texture
-	 * @param {Array} size
-	 * @param dimensions
-	 * @param {Array} output
-	 * @param {Object} webGl
-	 * @param {String} [type]
-	 */
-	constructor(texture, size, dimensions, output, webGl, type = 'NumberTexture') {
-		this.texture = texture;
-		this.size = size;
-		this.dimensions = dimensions;
-		this.output = output;
-		this.webGl = webGl;
-		this.kernel = null;
-		this.type = type;
-	}
+  /*
+   * 将gpu实例对象转换为数组形式，其中kernel数组每个元素对应GPU的一个运算核
+   */
+  toArray(gpu) {
+    if (!gpu)
+      throw new Error('You need to pass the GPU object for toArray to work.');
+    if (this.kernel) return this.kernel(this);
 
-	/**
-	 * @name toArray
-	 * @function
-	 * @memberOf Texture#
-	 *
-	 * @desc Converts the Texture into a JavaScript Array.
-	 * 
-	 * @param {Object} The `gpu` Object
-	 *
-	 */
-	toArray(gpu) {
-		if (!gpu) throw new Error('You need to pass the GPU object for toArray to work.');
-		if (this.kernel) return this.kernel(this);
+    // 这一段代码是核心功能
+    this.kernel = gpu
+      .createKernel(function(x) {
+        return x[this.thread.z][this.thread.y][this.thread.x];
+      })
+      .setOutput(this.output);
 
-		this.kernel = gpu.createKernel(function(x) {
-			return x[this.thread.z][this.thread.y][this.thread.x];
-		}).setOutput(this.output);
+    return this.kernel(this);
+  }
 
-		return this.kernel(this);
-	}
-
-	/**
-	 * @name delete
-	 * @desc Deletes the Texture.
-	 * @function
-	 * @memberOf Texture#
-	 *
-	 *
-	 */
-	delete() {
-		return this.webGl.deleteTexture(this.texture);
-	}
+  /**
+   * 删除WebGL上下文环境的纹理
+   */
+  delete() {
+    return this.webGl.deleteTexture(this.texture);
+  }
 };
